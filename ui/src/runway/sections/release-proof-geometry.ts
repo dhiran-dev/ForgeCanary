39644385ca86af42e506eb4fc9c3cab998c14b72
Dhiran <97387022+dhiran-dev@users.noreply.github.com>
2026-08-30 22:38:17 +0530

    fix(runway): align proof conduits with lead sockets (#11)

---
+++ ui/src/runway/sections/release-proof-geometry.ts
@@ -0,0 +1,15 @@
+export const releaseProofRoutePaths = {
+  current: 'M 796 195 H 842 Q 868 195 868 224 V 284 Q 868 309 895 309 H 915',
+  upgrade: 'M 796 438 H 849 Q 873 438 873 414 Q 873 383 899 383 H 915',
+  safety: 'M 1182 309 H 1211 Q 1237 309 1237 283 V 224 Q 1237 195 1264 195 H 1293',
+  reality: 'M 1182 383 H 1210 Q 1237 383 1237 409 Q 1237 438 1264 438 H 1293',
+  trunkLeft: 'M 1049 506 V 603 H 611',
+  trunkRight: 'M 1049 506 V 603 H 1418',
+  drop1: 'M 611 603 H 541 V 652',
+  drop2: 'M 720 603 V 652',
+  drop3: 'M 899 603 V 652',
+  drop4: 'M 1079 603 V 652',
+  drop5: 'M 1259 603 V 652',
+  drop6: 'M 1439 603 H 1419 V 652',
+  outcome: 'M 454 720 H 1642'
+} as const;
